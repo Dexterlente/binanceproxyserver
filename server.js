6f96baa407ec701636357a50303e42d9bd6ac10a
Dexter <dexter.lente18@gmail.com>
2024-08-06 17:05:38 +0800

    update port listen

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 // Dynamic WebSocket proxy
 app.use("/ws/:query", (req, res, next) => {
@@ -19,4 +20,8 @@ app.use("/ws/:query", (req, res, next) => {
 // Default route
 app.get("/", (req, res) => {
   res.send("WebSocket proxy server is running");
+});
+
+app.listen(port, () => {
+  console.log(`> Ready on http://localhost:${port}`);
 });
