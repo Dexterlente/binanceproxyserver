ebdf776c4a9aa98cfe52fbfd3f75299eed90d33d
Dexter <dexter.lente18@gmail.com>
2024-08-06 16:13:26 +0800

    initial commit binance proxy server to bypass ban

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+const app = express();
+
+// Dynamic WebSocket proxy
+app.use("/ws/:query", (req, res, next) => {
+  const query = req.params.query; // Get the dynamic part from the URL
+  const target = `wss://stream.binance.com:9443/ws/${query}@ticker`;
+
+  createProxyMiddleware({
+    target,
+    changeOrigin: true,
+    ws: true,
+    logLevel: "debug",
+  })(req, res, next);
+});
+
+// Default route
+app.get("/", (req, res) => {
+  res.send("WebSocket proxy server is running");
+});
+
+app.listen(3001, () => {
+  console.log("> Ready on http://localhost:3001");
+});
