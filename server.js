efc3d581d0afbc0a000d3e3aeedcebf1d5f2de63
Dexter <dexter.lente18@gmail.com>
2024-08-07 12:16:59 +0800

    update

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const setupWebSocketRoutes = require("./websockets");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 setupWebSocketRoutes(app);
 
