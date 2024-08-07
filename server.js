bc407370a00715326f1fde6836ad77a2bd047e2f
Dexter <dexter.lente18@gmail.com>
2024-08-07 11:00:28 +0800

    updated routes

--- server.js
+++ server.js
@@ -1,21 +1,10 @@
 const express = require("express");
-const { createProxyMiddleware } = require("http-proxy-middleware");
+const setupWebSocketRoutes = require("./websockets");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Dynamic WebSocket proxy
-app.use("/ws/:query", (req, res, next) => {
-  const query = req.params.query; // Get the dynamic part from the URL
-  const target = `wss://stream.binance.com:9443/ws/${query}@ticker`;
-
-  createProxyMiddleware({
-    target,
-    changeOrigin: true,
-    ws: true,
-    logLevel: "debug",
-  })(req, res, next);
-});
+setupWebSocketRoutes(app);
 
 // Default route
 app.get("/", (req, res) => {
